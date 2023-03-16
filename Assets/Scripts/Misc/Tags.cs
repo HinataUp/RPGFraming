7f78674a0a1fd5541c8a9aadbf5854ec13e68591
HinataUp <19965331661@163.com>
2023-03-16 14:46:47 +0800

    环境布置, 玩家摄像机跟随&边界设置,环境碰撞设置, 房屋添加

---
+++ Assets/Scripts/Misc/Tags.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tags {
+    public const string BoundsConfiner = "BoundsConfiner";
+}
---
+++ Assets/Scripts/Scene/SwitchConfineBoundingShape.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class SwitchConfineBoundingShape : MonoBehaviour {
+    void Start() {
+        SwitchBoundingShape();
+    }
+
+    /// <summary>
+    ///  Switch the collider that cinemachine users to define the edges of the screen 
+    /// </summary>
+    private void SwitchBoundingShape() {
+        PolygonCollider2D polygonCollider2D =
+            GameObject.FindWithTag(Tags.BoundsConfiner).GetComponent<PolygonCollider2D>();
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+        cinemachineConfiner.InvalidatePathCache();
+    }
+}
